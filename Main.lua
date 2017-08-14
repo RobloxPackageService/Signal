7ea1343332c3a65a2ceb9c6da521aa8ff5841290
Brad Sharp <BradSharp@users.noreply.github.com>
2017-08-14 10:48:23 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,37 +1,37 @@
-local event = {}
-event.__index = event
+local Signal = {}
+Signal.__index = Signal
 
-function event:Connect(listener)
+function Signal:Connect(listener)
 	return self.Bind.Event:connect(function (key)
 		listener(unpack(self.Cache[key]))
 	end)
 end
 
-function event:Wait()
+function Signal:Wait()
 	return unpack(self.Cache[self.Bind.Event:wait()])
 end
 
-function event:Fire(...)
+function Signal:Fire(...)
 	local key = tick()
 	self.Cache[key] = {...}
 	self.Bind:Fire(key)
 	self.Cache[key] = nil
 end
 
-event.connect = event.Connect
-event.wait = event.Wait
+Signal.connect = Signal.Connect
+Signal.wait = Signal.Wait
 
-function event:Destroy()
+function Signal:Destroy()
 	self.Bind:Destroy()
 	self.Bind = nil
 	self.Cache = nil
 end
 
-function event.new()
+function Signal.new()
 	return setmetatable({
 		Bind = Instance.new("BindableEvent"),
 		Cache = {},
-	}, event)
+	}, Signal)
 end
 
-return event
+return Signal
